806b0fe0badbfdf1759025f3e630bd76394e471c
credosky <draykons@yandex.ru>
2017-03-19 16:26:05 +0300

    web delete for url

--- config/web.php
+++ config/web.php
@@ -10,6 +10,7 @@ $config = [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
             'cookieValidationKey' => 'frsgFxEql5a-NXHlNE8EpNo2-UzaZXP9',
+			'baseUrl'=> '',
         ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
@@ -38,14 +39,14 @@ $config = [
             ],
         ],
         'db' => require(__DIR__ . '/db.php'),
-        /*
+        
         'urlManager' => [
             'enablePrettyUrl' => true,
-            'showScriptName' => false,
+            'showScriptName' => true,
             'rules' => [
             ],
         ],
-        */
+        
     ],
     'params' => $params,
 ];
